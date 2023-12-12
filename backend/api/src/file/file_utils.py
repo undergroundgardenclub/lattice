e2e5136519d2654c572200d2ca3c5e37d6a9e95d
Mark <x@markthemark.com>
2023-12-12 00:39:19 -0500

    feat(cv): media duration subprocess runner

--- backend/api/src/file/file_utils.py
+++ backend/api/src/file/file_utils.py
@@ -1,7 +1,9 @@
 import os
 import shutil
+import subprocess
 import tempfile
 
+# TMP
 def tmp_file_set(file_bytes):
     # Write the video bytes to a temporary file
     with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4', mode='wb') as tmp_file:
@@ -14,3 +16,15 @@ def tmp_file_rmv(file_path):
     print(f'[tmp_file_rmv] path: {file_path}')
     os.remove(file_path)
 
+
+# MEDIA
+def get_media_duration(file_path):
+    print("[get_video_duration] file_path:", file_path)
+    command = [
+        'ffprobe', '-v', 'error', '-show_entries', 'format=duration',
+        '-of', 'default=noprint_wrappers=1:nokey=1', file_path
+    ]
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    duration = float(result.stdout)
+    print(f"[get_video_duration] duration = {duration}, for file_path:", file_path)
+    return duration
