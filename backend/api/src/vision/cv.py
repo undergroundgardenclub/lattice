3f56af30497e9742dd6c4c68d0e141911759d568
Mark <x@markthemark.com>
2024-01-01 14:14:23 -0500

    feat(cv): is image blurry checker fn

--- backend/api/src/vision/cv.py
+++ backend/api/src/vision/cv.py
@@ -1,19 +1,19 @@
 import base64
-from io import BytesIO
 import cv2
+from io import BytesIO
 import numpy as np
 
 
 # ENCODINGS
 def encode_frame_to_jpg(frame, quality: int = 75) -> np.ndarray:
-    print("[encode_frame_to_jpg] encoding image to JPG")
+    print("[encode_frame_to_jpg] encoding")
     success, encoded_image = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), quality]) # default compression is 95 https://stackoverflow.com/questions/40768621/python-opencv-jpeg-compression-in-memory
     if not success:
         raise ValueError("Could not encode image")
     return encoded_image # numpy.ndarray
 
 # TODO: make this work w/ arrays or pyfiles or other things
-def encoded_frame_to_base64(image_ndarray: np.ndarray):
+def encoded_frame_jpg_to_base64(image_ndarray: np.ndarray):
     print("[encoded_image_to_base64] encoding image to base64", image_ndarray)
     buffered = BytesIO(image_ndarray)
     return base64.b64encode(buffered.getvalue()).decode('utf-8')
@@ -43,14 +43,14 @@ def video_frame_at_second(file_path, second) -> np.ndarray:
         print("[video_frame_at_second] error: ", err)
         return None
 
-def video_frame_generator(file_path, start_second=0, stop_second=None, interval_seconds=1):
+def video_frame_generator(file_path, start_second: int = None, stop_second: int = None, interval_seconds:int = 1):
     print(f"[video_frame_generator] generating frames from {start_second}->{stop_second}, 1 frame per {interval_seconds} seconds")
     # --- open file
     video = cv2.VideoCapture(file_path)
     # --- get/set frames info
     fps = video.get(cv2.CAP_PROP_FPS)
     total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-    start_frame = int(start_second * fps)
+    start_frame = int(start_second * fps) if start_second else 0
     stop_frame = int(stop_second * fps) if stop_second else total_frames
     frame_step = 1 if interval_seconds == None else int(interval_seconds * fps) # if no interval by seconds, output each frame
     # --- set starting frame
@@ -65,3 +65,16 @@ def video_frame_generator(file_path, start_second=0, stop_second=None, interval_
         current_frame += frame_step
     # --- release the video capture object
     video.release()
+
+def is_image_blurry(image_data):
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(image_data, cv2.COLOR_BGR2GRAY)
+    # Compute the Laplacian of the image and then return the focus measure, which is simply the variance of the Laplacian
+    variance_of_laplacian = cv2.Laplacian(gray, cv2.CV_64F).var()
+    # Define a threshold value for the variance; this can be tuned (example pics of thresholds: https://pyimagesearch.com/2015/09/07/blur-detection-with-opencv/)
+    threshold = 150.0
+    # If the variance is less than the threshold, the image is considered blurry
+    if variance_of_laplacian < threshold:
+        return True #, variance_of_laplacian
+    else:
+        return False #, variance_of_laplacian
